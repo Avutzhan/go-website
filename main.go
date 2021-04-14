9dde7cf56dc8f1ca1864a4b114d53351524d7a1c
Dautov Avutzhan <sd3@vrcode.kz>
2021-04-14 10:12:49 +0600

    news site set up structure server set up home page templates dinamic templates static files

--- main.go
+++ main.go
@@ -1,46 +1,25 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	_ "github.com/go-sql-driver/mysql"
+	"html/template"
+	"net/http"
 )
 
-type User struct {
-	Name string `json:"name"`
-	Age  uint16 `json:"age"`
+func index(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	t.ExecuteTemplate(w, "index", nil)
+}
+
+func handleFunc() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
 }
 
 func main() {
-	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/golang")
-	//db, err := sql.Open("mysql", "root:root@/golang")
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	//установка данных создание нового пользователя
-	//insert, err := db.Query("INSERT INTO `users` (`name`, `age`) VALUES('Alex ti v poryadke', 25)")
-	//if err != nil {
-	//	panic(err)
-	//}
-	//defer insert.Close()
-
-	res, err := db.Query("SELECT `name`, `age` FROM `users`")
-	if err != nil {
-		panic(err)
-	}
-
-	//Next если есть строка которую можно еще обработать вернет true если строки ольше нет то false
-	for res.Next() {
-		var user User
-		//Scan существуют ли данные в текущем ряде
-		//то есть тут мы вытягиваем данные с каждой строки и добавояем их в обьект который мы создали тут
-		err = res.Scan(&user.Name, &user.Age)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(fmt.Sprintf("User %s with age %d", user.Name, user.Age))
-	}
-
+	handleFunc()
 }
